e2eec5e6e4561f995852935c692dbe506f05f078
Okan Candeniz <candenizokan@gmail.com>
2023-07-30 13:41:02 +0300

    ctor connection

--- Identity/Context/ApplicationDbContext.cs
+++ Identity/Context/ApplicationDbContext.cs
@@ -1,9 +1,16 @@
 ﻿using Identity.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
 namespace Identity.Context
 {
     public class ApplicationDbContext : IdentityDbContext<AppUser>
     {
+        //bu sınıf context sınıfımdır. ctorda apsettinsden connectionstring lazım
+
+        public ApplicationDbContext(DbContextOptions)
+        {
+
+        }
     }
 }
