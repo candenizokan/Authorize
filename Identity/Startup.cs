26945d99661031ecc6bc1aaeaa17f40ef23f40f9
Okan Candeniz <candenizokan@gmail.com>
2023-07-30 13:57:41 +0300

    services.AddIdentity<AppUser, IdentityRole>

--- Identity/Startup.cs
+++ Identity/Startup.cs
@@ -1,6 +1,8 @@
-using Identity.Context;
+﻿using Identity.Context;
+using Identity.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -31,6 +33,27 @@ namespace Identity
                 (
                 options => options.UseSqlServer(Configuration.GetConnectionString("Default"))
                 );
+
+            services.AddIdentity<AppUser, IdentityRole>
+                (
+                    x =>
+                    {
+                        //burada giriş işlemkerş/kullanıcı detayları/şifrelere aidt detaylı isteklerimizi belirtebiliyoruz. Eğer yazmazsak defaulttaki ayarları kabul edecektir.
+
+                        x.SignIn.RequireConfirmedPhoneNumber = false;// girişte doğrulanmış telefon gerekli mi
+                        x.SignIn.RequireConfirmedEmail = false;// girişte doğrulanmış mail gerekli mi
+                        x.User.RequireUniqueEmail = true;//eşsiz mail olsun mu kişiye ait
+                        x.Password.RequiredLength = 4;//şifre kaç karakter olsun
+                        x.Password.RequireLowercase = false;//şifrede küçük harf zorunlu mu
+                        x.Password.RequireUppercase = false;//şifrede büyük harf zorunlu mu
+                        x.Password.RequireNonAlphanumeric = false;//şifrede noktalama işareti zorunlu mu
+                        x.Password.RequireDigit = false;//şifrede rakam zorunlu mu
+                        x.Password.RequiredUniqueChars = 0;//eşsiz karakter gerekli mi
+
+                        // gibi gibni daha birçok ayarlama yapılabilir. biz değer atamazsak o defaulttaki değerleri kabul eder. Yer yer bu sebepten hatalar alınabilir. dikkatten kaçan yerler olursa.
+                    }
+                ).AddEntityFrameworkStores<ApplicationDbContext>()//sayesinde aslında AppUser arkaplanda tutuluyor. Tablosuz ulaşılmış oluyor.
+                .AddDefaultTokenProviders();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
