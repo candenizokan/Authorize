9cf6f08c75b5d5a72d2def6f8bace99eac9b31e2
Okan Candeniz <candenizokan@gmail.com>
2023-08-05 10:17:58 +0300

    RegisterDTO model

--- Identity/Models/DTOs/RegisterDTO.cs
+++ Identity/Models/DTOs/RegisterDTO.cs
@@ -2,5 +2,9 @@
 {
     public class RegisterDTO
     {
+        //bunların hepsi IdentyUser de var ama ben dto oluşturuyorum
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Mail { get; set; }
     }
 }
