47bc044f0c15418bd5b192e27b1eb09f10f063d7
Okan Candeniz <candenizokan@gmail.com>
2023-08-05 10:49:49 +0300

    async Task<IActionResult>

--- Identity/Controllers/AccountController.cs
+++ Identity/Controllers/AccountController.cs
@@ -2,6 +2,7 @@
 using Identity.Models.DTOs;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace Identity.Controllers
 {
@@ -19,12 +20,13 @@ namespace Identity.Controllers
         }
 
         [HttpPost]
-        public IActionResult Register(RegisterDTO dto)
+        public async Task<IActionResult> Register(RegisterDTO dto)
         {
             if (ModelState.IsValid)// validasyonlarım tamam mı
             {
                 //herşey yolundaysa artık veritabanıma kaydedebilirim
-                AppUser appUser = new AppUser() { Email=dto.Mail, UserName=dto.UserName};
+                AppUser appUser = new AppUser() { Email = dto.Mail, UserName = dto.UserName };
+                IdentityResult result = await _userManager.CreateAsync(appUser, dto.Password);
             }
             return View(dto);
         }
