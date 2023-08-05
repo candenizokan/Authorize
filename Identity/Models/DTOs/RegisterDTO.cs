38e23282e284ff025001bffba1b15281ae87db2c
Okan Candeniz <candenizokan@gmail.com>
2023-08-05 10:21:04 +0300

    validasyon ekledim modelime DataAnnotations ile

--- Identity/Models/DTOs/RegisterDTO.cs
+++ Identity/Models/DTOs/RegisterDTO.cs
@@ -1,10 +1,22 @@
-﻿namespace Identity.Models.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Identity.Models.DTOs
 {
     public class RegisterDTO
     {
-        //bunların hepsi IdentyUser de var ama ben dto oluşturuyorum
+        //bunların hepsi IdentyUser de var ama ben dto oluşturuyorum.
+        //burada gerkli validasyonlarımı yapacağım
+
+        [Required(ErrorMessage ="Kullanıcı Adı Yazınız")]
+        [MinLength(3,ErrorMessage ="En az 3 karakter yazmalısınız")]
         public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Şifrenizi yazınız")]
+        [MaxLength(4, ErrorMessage = "Max 4 karakter yazmalısınız")]
         public string Password { get; set; }
+
+        [Required(ErrorMessage = "Mailinizi")]
+        [DataType(DataType.EmailAddress)]
         public string Mail { get; set; }
     }
 }
