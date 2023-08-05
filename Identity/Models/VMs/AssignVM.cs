eded548b87780733378db43eaa36a2d7e50757ef
Okan Candeniz <candenizokan@gmail.com>
2023-08-05 13:52:59 +0300

    AssignVM models

--- Identity/Models/VMs/AssignVM.cs
+++ Identity/Models/VMs/AssignVM.cs
@@ -1,6 +1,15 @@
-﻿namespace Identity.Models.VMs
+﻿using System.Collections.Generic;
+
+namespace Identity.Models.VMs
 {
     public class AssignVM
     {
+        public string RoleName { get; set; }
+
+        public IEnumerable<AppUser> HasRole { get; set; }//role sahip olanlar
+        public IEnumerable<AppUser> HasNotRole { get; set; }//role sahip olmayanlar
+
+        public string[] AddIds { get; set; }//role yeni eklenecekler
+        public string[] DeleteIds { get; set; }//mevcutta bu rolde olup silinenlerin idleri
     }
 }
