c4b5e0a7cc6f97f39ccdd1f231caed7454ac76dd
Okan Candeniz <candenizokan@gmail.com>
2023-08-05 12:54:15 +0300

    post create

--- Identity/Controllers/RoleController.cs
+++ Identity/Controllers/RoleController.cs
@@ -5,6 +5,12 @@ namespace Identity.Controllers
     public class RoleController : Controller
     {
         public IActionResult Create() => View(); //{ return yerine =>}
+
+        [HttpPost]
+        public IActionResult Create(string name)
+        {
+
+        }
        
     }
 }
