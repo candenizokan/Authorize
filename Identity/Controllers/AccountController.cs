ac8f49969237f95dfd783de22651dad19962cb19
Okan Candeniz <candenizokan@gmail.com>
2023-08-05 10:44:44 +0300

    UserManager<AppUser> _userManager; ile kendi yazmış olduğum repoya ulaşmışım gibi davranacak

--- Identity/Controllers/AccountController.cs
+++ Identity/Controllers/AccountController.cs
@@ -7,9 +7,11 @@ namespace Identity.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly UserManager<AppUser> _userManager;
+
         public AccountController(UserManager<AppUser> userManager)
         {
-
+            _userManager = userManager;
         }
         public IActionResult Register()
         {
