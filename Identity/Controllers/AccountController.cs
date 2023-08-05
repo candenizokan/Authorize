7379fd9e8af9add8ef0380df1847879c710f15df
Okan Candeniz <candenizokan@gmail.com>
2023-08-05 10:34:00 +0300

    [HttpPost]         public IActionResult Register(RegisterDTO dto)

--- Identity/Controllers/AccountController.cs
+++ Identity/Controllers/AccountController.cs
@@ -1,10 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Identity.Models.DTOs;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Identity.Controllers
 {
     public class AccountController : Controller
     {
         public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(RegisterDTO dto)
         {
             return View();
         }
