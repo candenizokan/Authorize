eb11a6b2ca88beec6d6043f4e2b3cbc57d882910
Okan Candeniz <candenizokan@gmail.com>
2023-08-05 12:40:36 +0300

    add rolecontroller

---
+++ Identity/Controllers/RoleController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Identity.Controllers
+{
+    public class RoleController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
