a7a4dda97fa7032090119c5592f1530852c2eb4f
Okan Candeniz <candenizokan@gmail.com>
2023-08-05 11:31:28 +0300

    IActionResult Login

--- Identity/Controllers/AccountController.cs
+++ Identity/Controllers/AccountController.cs
@@ -35,5 +35,10 @@ namespace Identity.Controllers
             }
             return View(dto);
         }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
     }
 }
